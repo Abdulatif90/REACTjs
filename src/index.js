5cb45df2b2f66829298cc01b0ad4101720b37ab0
Abdulatif <abdulatifsh90@gmail.com>
2025-05-06 14:26:29 +0900

    initial commits i REACTjs

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const el = (
+    <div>
+        <h1>
+            Hi Abdulatif!!!
+        </h1>
+        <input type="text" />
+        <button>Click Me</button>
+    </div>
+)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(el);
+
+
